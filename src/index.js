e29901aed1ecf91dc7055f7dc2d5f4e31ce9790a
chaos <huangchsh@ersoft.io>
2019-06-27 18:51:55 +0800

    网站移植

--- src/index.js
+++ src/index.js
@@ -7,20 +7,89 @@ import Filter from "./components/Filter"
 import Questions from "./components/Questions"
 import Footer from "./components/Footer"
 
+import questions from "./data/questions.json"
+import { scrollToTop } from "./utils"
+
+
 // 样式
 import "./styles/index.scss"
 
 // 注册
-app({
-  init: {},
-  view: state => (
-    <div>
-      {/* <BackToTopButton /> */}
-      <Header />
-      {/* <Filter /> */}
-      {/* <Questions /> */}
-      {/* <Footer /> */}
-    </div>
-  ),
-  node: document.getElementById("app")
+
+const state = {
+  questions,
+  filter: {
+    category: "All",
+    expertise: "Junior",
+    sortAscending: true,
+    dropdown: {
+      name: "Category",
+      items: [
+        {
+          name: "All",
+          icon: "check"
+        },
+        {
+          name: "HTML",
+          icon: "layout"
+        },
+        {
+          name: "CSS",
+          icon: "feather"
+        },
+        {
+          name: "JavaScript",
+          icon: "code"
+        },
+        {
+          name: "React",
+          icon: "globe"
+        },
+        {
+          name: "Node",
+          icon: "server"
+        },
+        {
+          name: "Security",
+          icon: "shield"
+        }
+      ]
+    }
+  }
+}
+
+const actions = {
+  scrollBackToTop: scrollToTop,
+  onAnswerButtonClick: ({ name, isOpen }) => state => ({
+    questions: state.questions.map(
+      q => (q.name === name ? { ...q, isOpen: !isOpen } : q)
+    )
+  }),
+  filter: {
+    onSortDirectionChange: () => state => ({
+      sortAscending: !state.sortAscending
+    }),
+    set: name => ({ category: name })
+  }
+}
+
+const view = (state, actions) => (
+  <div>
+    <BackToTopButton />
+    <Header />
+    <Filter />
+    <Questions />
+    <Footer />
+  </div>
+)
+
+app(state, actions, view, document.body)
+
+setTimeout(() => {
+  const backToTopButton = document.querySelector(".BackToTopButton")
+  addEventListener("scroll", () => {
+    backToTopButton.classList[window.scrollY > 750 ? "add" : "remove"](
+      "is-visible"
+    )
+  })
 })
